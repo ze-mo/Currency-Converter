922f7128be262f57e04144044a08bdaaf1e1ba59
ze-mo <iliyan.iliev223@gmail.com>
2022-02-08 01:35:38 +0200

    Added title key to render context

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -14,7 +14,7 @@ def register(request):
     else:
         form = UserRegisterForm()
 
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'users/register.html', {'form': form, 'title': 'Register'})
 
 @login_required
 def profile(request):
@@ -33,7 +33,8 @@ def profile(request):
 
     context = {
         'u_form': u_form,
-        'p_form': p_form
+        'p_form': p_form,
+        'title': 'Profile'
     }
 
     return render(request, 'users/profile.html', context)

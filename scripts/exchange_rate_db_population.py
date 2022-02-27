1dcc757ac142b9b77b9a115b6c78589b01147ea6
ze-mo <iliyan.iliev223@gmail.com>
2022-02-27 13:17:35 +0200

    Update script to save only the most recent exchange rate

--- scripts/exchange_rate_db_population.py
+++ scripts/exchange_rate_db_population.py
@@ -1,13 +1,11 @@
 import requests
 import time
 import itertools
-from datetime import datetime
-import datetime
 import logging
 import os
 import psycopg2
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
@@ -38,35 +36,21 @@ for perm_tuple in list_of_permutations:
 
 def populate_db(pairs):
     """Submits a request to the Alphavantage API with the parameters set for each permutation of the currencies list.
-    After the data is retrieved, it's upserted into the rates_by_pairs table, where each record represents a currency pair 
-    with its exchange rate at a 15 minute interval."""
+    After the data is retrieved, it's upserted into the forex_ratesbypairs table, where each record represents a currency pair 
+    with its exchange rate. Realtime rates are updated every 6 minutes."""
 
-
-    mycursor.execute("TRUNCATE TABLE forex_ratesbypairs")
-    conn.commit()
-    
-    """counter = 0
+    counter = 0
     for pair in pairs:
-        url = f'https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol={pair[:3]}&to_symbol={pair[3:]}&interval=15min&outputsize=compact&apikey={os.environ.get("FOREX_API_KEY")}'
+        url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={pair[:3]}&to_currency={pair[3:]}&apikey={os.environ.get('FOREX_API_KEY')}"
         r = requests.get(url)
         data_dict = r.json()
         logger.info(r.status_code)
         try:
-            values_by_date = data_dict["Time Series FX (15min)"]
-            last_refreshed_api = data_dict["Meta Data"]["4. Last Refreshed"]
-            last_refresh_date = datetime.datetime.strptime(last_refreshed_api, '%Y-%m-%d %H:%M:%S')
-            for key, value in values_by_date.items():
-                key_date_obj = datetime.datetime.strptime(key, '%Y-%m-%d %H:%M:%S')
-                tdelta = datetime.timedelta(minutes=45)
-                limit = last_refresh_date - tdelta
-                if key_date_obj > limit:
-                    logger.info(f'{pair} | {key_date_obj} not included')
-                    continue
-                else:
-                    refreshed = key
-                    close_value = value["4. close"]
-                    mycursor.execute(f"INSERT INTO forex_ratesbypairs (pair, last_refreshed, exchange_rate) VALUES ('{pair}', '{refreshed}', {close_value})")
-                    conn.commit()
+            realtime_rate = data_dict["Realtime Currency Exchange Rate"]
+            last_refreshed_value = realtime_rate["6. Last Refreshed"]
+            exchange_rate = realtime_rate["5. Exchange Rate"]
+            mycursor.execute(f"INSERT INTO forex_ratesbypairs (pair, last_refreshed, exchange_rate) VALUES ('{pair}', '{last_refreshed_value}', {exchange_rate})")
+            conn.commit()
             counter += 1
             logger.info(f'{pair} Added... ')
 
@@ -77,9 +61,8 @@ def populate_db(pairs):
         if counter == 5:
             logger.info('Sleeping 60 seconds... ')
             time.sleep(60)
-            counter = 0"""
+            counter = 0
 
 if __name__ == "__main__":
-    populate_db(set_of_paired_currencies)
-    logger.info("Finished")
-    mycursor.close()
+    while True:
+        populate_db(set_of_paired_currencies)
